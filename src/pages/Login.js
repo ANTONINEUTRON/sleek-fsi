f60e4f38d59531daf844ec21d8687c5593450bd1
AntoniNeutron <antonineutron@gmail.com>
2022-08-25 23:02:51 +0100

    minor tins

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,16 @@
 export default function Login(){
     return (
-        <div>
-            Login Page
+        <div className="ml-36 mr-36 mt-3 p-7 bg-white rounded-md">
+            <div className="flex flex-col items-center">
+                Logo
+            </div>
+            <div className="flex flex-col mt-4">
+                <label for="org_id">Organization ID</label>
+                <input type={"text"} name="org_id"/>
+                <label for="password">Password</label>
+                <input type={"password"} name="org_id"/>
+                <input type={"submit"} className="bg-blue-600 rounded-lg ml-25 mr-25 w-24 mt-10 text-bold m-auto text-white p-3"/>
+            </div>
         </div>
     );
 }
